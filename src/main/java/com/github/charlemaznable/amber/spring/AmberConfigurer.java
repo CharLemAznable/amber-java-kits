22718c1b53a2ebd1d76367c843aae125afebb360
CharLemAznable <545541819@qq.com>
2023-01-19 01:49:17 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/amber/spring/AmberConfigurer.java
+++ src/main/java/com/github/charlemaznable/amber/spring/AmberConfigurer.java
@@ -2,20 +2,28 @@ package com.github.charlemaznable.amber.spring;
 
 import com.github.charlemaznable.amber.config.AmberConfig;
 import com.github.charlemaznable.core.spring.ElvesImport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Nonnull;
-import java.util.function.Supplier;
 
 import static com.github.charlemaznable.configservice.ConfigFactory.getConfig;
-import static com.github.charlemaznable.core.spring.SpringContext.getBeanOrAutowire;
+import static com.github.charlemaznable.core.lang.Condition.nullThen;
 
 @Configuration
 @ElvesImport
 public class AmberConfigurer implements WebMvcConfigurer {
+
+    private final AmberConfig amberConfig;
+
+    @Autowired
+    public AmberConfigurer(@Nullable AmberConfig amberConfig) {
+        this.amberConfig = nullThen(amberConfig, () -> getConfig(AmberConfig.class));
+    }
 
     @Override
     public void addInterceptors(@Nonnull InterceptorRegistry registry) {
@@ -24,20 +32,12 @@ public class AmberConfigurer implements WebMvcConfigurer {
 
     @Bean("com.github.charlemaznable.amber.spring.AmberInterceptor")
     public AmberInterceptor amberInterceptor() {
-        return new AmberInterceptor(amberConfig());
+        return new AmberInterceptor(amberConfig);
     }
 
     @Bean("com.github.charlemaznable.amber.spring.AmberCocsHandler")
     public AmberCocsHandler amberCocsHandler() {
-        return new AmberCocsHandler(amberConfig());
-    }
-
-    private AmberConfig amberConfig() {
-        return getBeanOrAutowire(AmberConfig.class, defaultAmberConfigSupplier());
-    }
-
-    private Supplier<AmberConfig> defaultAmberConfigSupplier() {
-        return () -> getConfig(AmberConfig.class);
+        return new AmberCocsHandler(amberConfig);
     }
 }
 
